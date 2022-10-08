2020f2105583c9e8f3cd958b7792ff60772a1a6e
jpcortesgd <jpcortesg@eafit.edu.co>
2022-10-08 05:50:21 -0500

    Create super user

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py createsuperuser
+jpcortesg
+jpcortesg@hotmail.com
+jpcortesg123.
+jpcortesg123.
